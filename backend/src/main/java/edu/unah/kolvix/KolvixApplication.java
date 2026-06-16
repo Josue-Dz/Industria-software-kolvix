3ebdf5577f1a7b818a39981b5e02f2a3a65f8615
Josue-Dz <ronnyjosueposadas4@gmail.com>
2026-06-16 15:39:09 -0600

    Se crean frontend y backend

---
+++ backend/src/main/java/edu/unah/kolvix/KolvixApplication.java
@@ -0,0 +1,13 @@
+package edu.unah.kolvix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolvixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolvixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/unah/kolvix/KolvixApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unah.kolvix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolvixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
